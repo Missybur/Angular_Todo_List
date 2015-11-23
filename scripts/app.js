9a4ee727af7d604c89a4eedf2f0f3d16907b8748
Missy Bur <missybur@gmail.com>
2015-11-22 19:35:19 -0800

    Add save and delete functions to angular todo app

--- scripts/app.js
+++ scripts/app.js
@@ -1,17 +1,46 @@
 angular.module("todoListApp", [])
-.controller("mainCtrl", function($scope){
-  $scope.learningNgChange = function() {
-    console.log("Input changed")
+
+.controller("mainCtrl", function($scope, dataService){
+  $scope.addTodo = function(){
+    var todo = {name: "This is a new todo"};
+    $scope.todos.push(todo);
   };
 
 
-  $scope.todos = [
-  {"name": "clean the house"},
-  {"name": "do the laundry"},
-  {"name": "go to post office"},
-  {"name": "walk the dog"},
-  {"name": "iron"}
-  ]
+  $scope.helloWorld = dataService.helloWorld;
 
+  dataService.getTodos(function(response){
+    console.log(response.data);
+    $scope.todos = response.data;
+  })
 
+  $scope.deleteTodo = function(todo, $index) {
+    dataService.deleteTodo(todo)
+    $scope.todos.splice($index, 1)
+  }
+
+  $scope.saveTodo = function(todo) {
+    dataService.saveTodo(todo)
+  }
+
+})
+
+.service("dataService", function($http){
+
+  this.helloWorld = function(){
+    console.log("This is the hello service fucntion.");
+  };
+
+  this.getTodos = function(callback) {
+    $http.get("mock/todos.json")
+  .then(callback)
+};
+
+this.deleteTodo = function(todo){
+  console.log("The" + todo.name + " has been deleted")
+};
+
+this.saveTodos = function(todo) {
+  console.log("The " + todo.name + " was saved");
+}
 });
